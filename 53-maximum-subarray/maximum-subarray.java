7d681b2b758422969ff671b96fc8ea63ec919279
Vishal <vishalbharati512@gmail.com>
2026-03-27 03:12:20 +0530

    Time: 1 ms (99.93%) | Memory: 80.7 MB (9.12%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -3,9 +3,14 @@ class Solution {
         int curr_sum = 0;
         int max_sum= Integer.MIN_VALUE;
         for(int i =0; i<nums.length; i++){
+            curr_sum = curr_sum+nums[i];
+            if(curr_sum > max_sum){
+                max_sum = curr_sum;
+            }
+            if(curr_sum < 0 ){
+                curr_sum = 0;
+            }
             
-            curr_sum =Math.max(nums[i],curr_sum+nums[i]);
-            max_sum = Math.max(curr_sum,max_sum);
             
             
         }
