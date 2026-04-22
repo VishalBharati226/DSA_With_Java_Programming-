3f18bff760a16620692a070fdfec4b4d4c8062ad
Vishal <vishalbharati512@gmail.com>
2026-04-22 08:05:02 +0530

    Time: 1 ms (100.00%) | Memory: 43.8 MB (20.85%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.java
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -1,26 +1,35 @@
+enum Result {
+    TRUE, FALSE
+}
+
 class Solution {
-    	public boolean isMatch(String s, String p) {
-        return isMatch(0,s,0,p);
+    Result[][] memo;
+
+    public boolean isMatch(String text, String pattern) {
+        memo = new Result[text.length() + 1][pattern.length() + 1];
+        return dp(0, 0, text, pattern);
     }
-    private boolean isMatch(int i, String s, int j, String p) { 
-        int sn = s.length(), pn = p.length();
-        if(j==pn) { // since * in p can match 0 of previous char, so empty string(i==sn) may match p
-            return i==sn;    
+
+    public boolean dp(int i, int j, String text, String pattern) {
+        if (memo[i][j] != null) {
+            return memo[i][j] == Result.TRUE;
         }
-        char pj = p.charAt(j);
-        if(j+1<pn && p.charAt(j+1)=='*') { //match *, needs to look at the next char to repeate current char
-            if(isMatch(i,s,j+2,p)) {
-                return true;
+        boolean ans;
+        if (j == pattern.length()){
+            ans = i == text.length();
+        } else{
+            boolean first_match = (i < text.length() &&
+                                   (pattern.charAt(j) == text.charAt(i) ||
+                                    pattern.charAt(j) == '.'));
+
+            if (j + 1 < pattern.length() && pattern.charAt(j+1) == '*'){
+                ans = (dp(i, j+2, text, pattern) ||
+                       first_match && dp(i+1, j, text, pattern));
+            } else {
+                ans = first_match && dp(i+1, j+1, text, pattern);
             }
-            while(i<sn && (pj == '.'||pj==s.charAt(i))) {
-                if(isMatch(++i,s,j+2,p)) {
-                    return true;
-                }
-            }
-        } else if(i<sn && (s.charAt(i) == pj ||    //match char
-                   pj=='.')) {              //match dot
-            return isMatch(i+1, s, j+1, p);
         }
-        return false;
+        memo[i][j] = ans ? Result.TRUE : Result.FALSE;
+        return ans;
     }
 }
